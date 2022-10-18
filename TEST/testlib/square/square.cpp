e816b7cbee82edfd1047f2ee137b073aab9ae809
EfthyC <erothsh@yahoo.gr>
2022-10-18 16:27:09 +0200

    Riempito le cartelle square e bin/TEST.

---
+++ TEST/testlib/square/square.cpp
@@ -0,0 +1,11 @@
+#include "testlib.h"
+
+TestLib::TestLib()
+{
+}
+
+
+float TestLib::test(float a)
+{
+    return a*a;
+}
---
+++ TEST/testlib/square/square.h
@@ -0,0 +1,13 @@
+#ifndef TESTLIB_H
+#define TESTLIB_H
+
+
+class  TestLib
+{
+public:
+    TestLib();
+
+    float test(float a);
+};
+
+#endif // TESTLIB_H
